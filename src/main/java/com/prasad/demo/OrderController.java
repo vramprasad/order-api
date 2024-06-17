cb6062b208fd2b7507bd7cf675504abd192f282e
Ramprasad V <vrprasad@gmail.com>
2024-06-17 10:14:08 +0530

    Added port and context

--- src/main/java/com/prasad/demo/OrderController.java
+++ src/main/java/com/prasad/demo/OrderController.java
@@ -9,16 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 @Slf4j
 @RestController
 public class OrderController {
 
     @GetMapping("/healthcheck")
-    ResponseEntity<String> sayHello() throws InterruptedException {
+    ResponseEntity<String> healthcheck() throws InterruptedException {
         String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
         log.info("Inside order-api --> healthcheck");
         String responseText = "Order API healthcheck @ " + timeStamp + " - All OK";
         return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.TEXT_PLAIN).body(responseText.toString());
+    }
+    @GetMapping("/createorder")
+    ResponseEntity<String> createOrder() throws InterruptedException {
+        log.info("Inside order-api --> createorder");
+        Random rand = new Random();
+        int orderNumber;
+        orderNumber = rand.nextInt(100000);
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.TEXT_PLAIN).body(String.valueOf(orderNumber));
     }
 }
